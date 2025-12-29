c1b71659b080a8f397e1de6cd94296945933973b
Khalil Ahmad Jahed <khalilahmadjahed2017@gmail.com>
2025-12-29 23:12:22 +0430

    Entity: User is created with its fields

---
+++ src/main/java/com/jahed/inventorymanagementsystem/entity/User.java
@@ -0,0 +1,60 @@
+package com.jahed.inventorymanagementsystem.entity;
+
+import com.jahed.inventorymanagementsystem.enums.UserRole;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotBlank(message = "Name is required!")
+    private String name;
+
+    @NotBlank(message = "email is required!")
+    @Column(unique = true)
+    private String email;
+
+    @NotBlank(message = "Password is required!")
+    private String password;
+
+    @NotBlank(message = "Phone Number is required!")
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    @OneToMany(mappedBy = "user")
+    private List<Transation> transactions;
+
+    @Column(name = "created_at")
+    private final LocalDateTime createdAt = LocalDateTime.now();
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", role=" + role +
+                ", createdAt=" + createdAt +
+                '}';
+    }
+}
