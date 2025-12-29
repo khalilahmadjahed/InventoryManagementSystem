165f3a6825e2d59a92f8c456702e167ba3d4ccb3
Khalil Ahmad Jahed <khalilahmadjahed2017@gmail.com>
2025-12-29 23:51:01 +0430

    Enrity: Transaction is created with its feilds

---
+++ src/main/java/com/jahed/inventorymanagementsystem/entity/Transaction.java
@@ -0,0 +1,66 @@
+package com.jahed.inventorymanagementsystem.entity;
+
+import com.jahed.inventorymanagementsystem.enums.TransactionStatus;
+import com.jahed.inventorymanagementsystem.enums.TransactionType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "transactions")
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private Integer totalProducts;
+
+    private BigDecimal totalPrice;
+
+    @Enumerated(EnumType.STRING)
+    private TransactionType transactionType;
+
+    @Enumerated(EnumType.STRING)
+    private TransactionStatus transactionStatus;
+
+    private String description;
+
+    private LocalDateTime updatedAt;
+
+    private final LocalDateTime createdAt = LocalDateTime.now();
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "supplier_id")
+    private Supplier supplier;
+
+    @Override
+    public String toString() {
+        return "Transaction{" +
+                "id=" + id +
+                ", totalProducts=" + totalProducts +
+                ", totalPrice=" + totalPrice +
+                ", transactionType=" + transactionType +
+                ", transactionStatus=" + transactionStatus +
+                ", description='" + description + '\'' +
+                ", updatedAt=" + updatedAt +
+                ", createdAt=" + createdAt +
+                '}';
+    }
+}
