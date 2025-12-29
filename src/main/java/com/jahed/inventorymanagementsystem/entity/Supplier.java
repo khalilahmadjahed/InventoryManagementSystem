683ee02d8dbe9e4a6adc9ede26341515372d4a4b
Khalil Ahmad Jahed <khalilahmadjahed2017@gmail.com>
2025-12-29 23:25:55 +0430

    Enrity: Supplier is created with its feilds

---
+++ src/main/java/com/jahed/inventorymanagementsystem/entity/Supplier.java
@@ -0,0 +1,26 @@
+package com.jahed.inventorymanagementsystem.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "suppliers")
+public class Supplier {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotBlank(message = "Name is required!")
+    private String name;
+
+    private String address;
+}
