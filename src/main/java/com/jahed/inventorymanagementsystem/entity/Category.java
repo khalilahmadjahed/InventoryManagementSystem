11167a1695a0b902235cffcf02fd0b03af7ab29e
Khalil Ahmad Jahed <khalilahmadjahed2017@gmail.com>
2025-12-29 23:20:51 +0430

    Enrity: Category is created with its feilds

---
+++ src/main/java/com/jahed/inventorymanagementsystem/entity/Category.java
@@ -0,0 +1,36 @@
+package com.jahed.inventorymanagementsystem.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotBlank(message = "Name is required!")
+    @Column(unique = true)
+    private String name;
+
+    @OneToMany(mappedBy = "category")
+    private List<Product> products;
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
