c1fc647f30bc815f01726e45df6d2635f1cfff48
Khalil Ahmad Jahed <khalilahmadjahed2017@gmail.com>
2025-12-30 12:46:58 +0430

    DTO: RegisterRequest is created as a RequestDTO for user registeration

---
+++ src/main/java/com/jahed/inventorymanagementsystem/dto/RegisterRequest.java
@@ -0,0 +1,23 @@
+package com.jahed.inventorymanagementsystem.dto;
+
+import com.jahed.inventorymanagementsystem.enums.UserRole;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+
+    @NotBlank(message = "Name is required")
+    private String name;
+    @NotBlank(message = "Email is required")
+    private String email;
+    @NotBlank(message = "Password is required")
+    private String password;
+    @NotBlank(message = "Phone Number is required")
+    private String phoneNumber;
+    private UserRole role;
+}
